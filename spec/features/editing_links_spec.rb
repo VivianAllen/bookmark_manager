c29b547d886648186674f92fb6c34874d671c1f4
Vivian Allen <mrvivianallen@gmail.com>
2018-03-15 10:41:36 +0000

    worked out how to test autofilled forms

--- spec/features/editing_links_spec.rb
+++ spec/features/editing_links_spec.rb
@@ -1,4 +1,12 @@
 feature 'editing a link' do
+  scenario 'editing a link, seeing default values' do
+    visit '/'
+    within '#link-1' do
+      click_on 'Edit'
+    end
+    expect(page).to have_selector("input[value='http://www.makersacademy.com']")
+    expect(page).to have_selector("input[value='Makers Academy']")
+  end
   scenario 'editing a link' do
     visit '/'
     within '#link-1' do
